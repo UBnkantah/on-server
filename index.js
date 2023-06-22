d49bb6cb4a47e09b146e926d2c15094f5a0ec723
UBnkantah <90422770+UBnkantah@users.noreply.github.com>
2023-06-22 03:59:42 +0100

    done

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const register = require("./routes/register");
+const login = require("./routes/login");
+const stripe = require("./routes/stripe");
+
+const products = require("./products");
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/register", register);
+app.use("/api/login", login);
+app.use("/api/stripe", stripe);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our online shop API...");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
+const port = process.env.PORT || 5000;
+const url = process.env.DB_URL;
+
+app.listen(port, () => {
+  console.log(`Server is Running on Port ${port}`);
+});
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected Successfully"))
+  .catch((err) => console.log(err));
---
+++ products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/dr6a80sph/image/upload/v1686949761/iphone12pro_sbpuod.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    desc: "5.4-inch display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/dr6a80sph/image/upload/v1686949761/iphone12_opqcwq.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/dr6a80sph/image/upload/v1686949761/galaxyS_zs9fld.png",
+  },
+];
+
+module.exports = products;
